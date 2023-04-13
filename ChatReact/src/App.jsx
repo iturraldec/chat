18b62ed7f6f3803494877dac180fbc5b1c66ac9a
Carlos Iturralde <iturraldec@gmail.com>
2023-04-12 20:17:54 -0500

    Login. [React]

--- ChatReact/src/App.jsx
+++ ChatReact/src/App.jsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { SHA256 } from 'crypto-js';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [usuario, usuarioMan] = useState('');
+  const [password, passwordMan] = useState('');
+
+  function setUsuario(valor) {
+    usuarioMan(valor);
+  }
+
+  function setPassword(valor) {
+    passwordMan(valor);
+  }
+
+  function login() {
+    fetch('http://localhost:5133/api/access', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: usuario,
+        password: SHA256(password).toString()
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Container>
+      <Row className="mt-5 justify-content-center">
+        <Col xs={6}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" 
+                            placeholder="Enter email" 
+                            value={usuario} 
+                            onChange={(e) => setUsuario(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" 
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password" />
+            </Form.Group>
+            
+            <Button variant="primary" onClick={() => login()}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
-export default App
+export default App;
